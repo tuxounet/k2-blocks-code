e8dadf13e909fb036b4192046f82b5548c16e278
Christophe Tiraoui <tuxounet@gmail.com>
2023-02-05 18:44:42 +0100

    fix

--- blocks/spa/create-react-app/src/index.test.tsx
+++ blocks/spa/create-react-app/src/index.test.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 
-import App from "./elements/App";
 
+import <%= element %> from "./elements/<%= element %>"
+ 
 test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(<<%= element %> />);
+  const linkElement = screen.getByText(/ERROR/);
+  expect(linkElement).not.toBeInTheDocument();
 });
