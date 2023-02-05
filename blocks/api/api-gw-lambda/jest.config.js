261ce349c2714ce01f8d91b58efab55081a4b42b
Christophe Tiraoui <tuxounet@gmail.com>
2023-02-05 21:36:00 +0100

    ++

--- blocks/api/api-gw-lambda/jest.config.js
+++ blocks/api/api-gw-lambda/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
-  roots: ['<rootDir>/stacks'],
+  roots: ['<rootDir>/src'],
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
