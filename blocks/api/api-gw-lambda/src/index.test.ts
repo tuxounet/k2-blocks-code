3520d5a59ee0c613b6447a064abfd9cefa00050c
Christophe Tiraoui <tuxounet@gmail.com>
2023-02-05 21:05:55 +0100

    add base routes

--- blocks/api/api-gw-lambda/src/index.test.ts
+++ blocks/api/api-gw-lambda/src/index.test.ts
@@ -2,7 +2,33 @@ import { main } from "./index";
 
 describe("index", () => {
   test("first test", async () => {
-    const result = await main();
+    const result = await main(
+      {
+        httpMethod: "GET",
+        body: "",
+        headers: {},
+        multiValueHeaders: {},
+        path: "/",
+        pathParameters: {},
+        isBase64Encoded: false,
+        resource: "/",
+        queryStringParameters: {},
+        multiValueQueryStringParameters: {},
+        stageVariables: null,
+        requestContext: {
+          path: "",
+          apiId: "",
+          httpMethod: "GET",
+          stage: "test",
+          resourcePath: "/",
+          accountId: "",
+          protocol: "http",
+          requestId: "",
+          resourceId: "",
+        },
+      },
+      {}
+    );
     expect(result).toHaveProperty("status", 200);
   });
 });
--- blocks/api/api-gw-lambda/src/index.ts
+++ blocks/api/api-gw-lambda/src/index.ts
@@ -1,7 +1,11 @@
 import runtime, { types } from "@tuxounet-k2/runtime";
+import { Context, APIGatewayEvent, APIGatewayProxyResult } from "aws-lambda";
 
-export async function main(): Promise<types.api.IApiResult> {
-  runtime.log.info("greeting", "hello", "world");
-
+export async function main(
+  event: APIGatewayEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> {
+  console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+  console.log(`Context: ${JSON.stringify(context, null, 2)}`);
   return runtime.results.successApiResult("OK", { hello: "world" });
 }
