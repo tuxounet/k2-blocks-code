0e68cb583aacdede763fc387d5d138980b163a66
Christophe Tiraoui <tuxounet@gmail.com>
2023-02-05 21:18:35 +0100

    ++

--- blocks/api/api-gw-lambda/src/index.test.ts
+++ blocks/api/api-gw-lambda/src/index.test.ts
@@ -26,7 +26,7 @@ describe("index", () => {
           requestId: "",
           resourceId: "",
           requestTimeEpoch: 10,
-          authorizer: null, 
+          authorizer: null,
           identity: {
             accessKey: "",
             apiKey: "",
@@ -46,7 +46,30 @@ describe("index", () => {
           },
         },
       },
-      {}
+      {
+        awsRequestId: "",
+        functionName: "",
+        callbackWaitsForEmptyEventLoop: false,
+        functionVersion: "",
+        invokedFunctionArn: "",
+        logGroupName: "",
+        memoryLimitInMB: "",
+        logStreamName: "",
+        getRemainingTimeInMillis: () => {
+          return 12;
+        },
+        done: (e, r) => {
+          return;
+        },
+        fail: (e) => {
+          return;
+        },
+        succeed: (e) => {
+          return;
+        },
+        clientContext: undefined,
+        identity: undefined,
+      }
     );
     expect(result).toHaveProperty("status", 200);
   });
