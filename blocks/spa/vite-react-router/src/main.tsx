c09dd939dc262a2accda8afa9449ca3b22a90c14
Christophe Tiraoui <tuxounet@gmail.com>
2023-02-08 22:44:04 +0100

    basic routing

--- blocks/spa/vite-react-router/src/main.tsx
+++ blocks/spa/vite-react-router/src/main.tsx
@@ -2,13 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Default from "./routes/Default";
+
+<% for(const route of routes) { %>import <%= route.element %> from "./routes/<%= route.element %>";
+<% } %>
+
 
 const router = createBrowserRouter([
+  <% for(const route of routes) { %>
   {
-    path: "/",
-    element: <Default />,
+    path: "<%= route.path>",
+    element: <<%= route.element> />,
   },
+  <% } %>
 ]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
