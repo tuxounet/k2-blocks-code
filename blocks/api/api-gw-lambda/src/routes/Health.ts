30e34fb31371ba7dfb6ff25da788fc13d537cc3c
Christophe Tiraoui <tuxounet@gmail.com>
2023-02-11 12:08:48 +0100

    cleanup

--- blocks/api/api-gw-lambda/src/routes/Health.ts
+++
@@ -1,8 +0,0 @@
-import runtime, { types } from "@tuxounet-k2/runtime";
-const handler: types.api.IApiHandler = async (
-  request: types.api.IApiRequest
-): Promise<types.api.IApiResult> => {
-  return runtime.results.successApiResult("OK", { heathy: "OK" });
-};
-
-export default handler;
